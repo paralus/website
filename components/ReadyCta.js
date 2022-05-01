3bfbcbb58c9e6e8e8738ea2ef8f877fc5e5c9050
elenalape <hi@elenalape.com>
2022-05-01 22:10:16 +0100

    remove package-lock

--- components/ReadyCta.js
+++ components/ReadyCta.js
@@ -1,26 +1,25 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react'
+import Link from 'next/link'
 
 export default function ReadyCta() {
   return (
-    <div className="px-20 pt-20 pb-20 bg-slate-900">
+    <div className="bg-slate-900 px-20 pt-20 pb-20">
       <div className="flex flex-col justify-center gap-y-6">
-        <div className="text-5xl text-center font-bold">Try it yourself!</div>
+        <div className="text-center text-5xl font-bold">Try it yourself!</div>
         <div className="flex justify-center">
-          <div className="text-xl font-semibold text-center text-sky-200 subtitleTextWrapper">
-            Explore our documentation or check out our GitHub repo to get
-            started with RCloud and the Zero-Trust Access Service for
-            Kubernetes.
+          <div className="subtitleTextWrapper text-center text-xl font-semibold text-sky-200">
+            Explore our documentation or check out our GitHub repo to get started with RCloud and
+            the Zero-Trust Access Service for Kubernetes.
           </div>
         </div>
-        <div className="flex justify-center mt-5">
+        <div className="mt-5 flex justify-center">
           <Link href="/docs">
-            <button className="w-fit bg-sky-500 hover:bg-sky-700 text-white text-sm font-semibold py-2 px-4 rounded-md">
+            <button className="w-fit rounded-md bg-sky-500 py-2 px-4 text-sm font-semibold text-white hover:bg-sky-700">
               Get Started
             </button>
           </Link>
         </div>
       </div>
     </div>
-  );
+  )
 }
