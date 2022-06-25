bd156dcf8e37a6394e1b2720e276f997d1600be4
elenalape <hi@elenalape.com>
2022-06-25 14:09:39 -0400

    edit outline

--- src/components/Features.js
+++ src/components/Features.js
@@ -11,8 +11,8 @@ export default function Features() {
         <div className="space-y-20 lg:space-y-30 py-20" id="features">
           <Feature
             reverse={true}
-            title="Manage access to all your clusters from one centralized, free, platform"
-            description="Generate, maintain, and revoke kubeconfigs across any number of environments with one tool"
+            title="One place to manage all Kubernetes access"
+            description="Generate, maintain, and revoke access configurations across clusters, projects, namespaces, etc."
             image="authorization.svg"
             bullets={[
               "Keep your existing SSO or RBAC solution",
@@ -22,12 +22,12 @@ export default function Features() {
           />
           <Feature
             title="Zero-trust security by default"
-            description="ZTKA applies zero-trust security principles to secure Kubernetes access through kubectl"
+            description="Paralus applies zero-trust security principles to secure Kubernetes"
             image="new-role.svg"
             bullets={[
               "Keep your existing kubectl scripts and workflows",
               "Control access with pre-configured roles",
-              "Dynamically change permissions and their duration",
+              "Dynamically revoke or change permissions and their duration",
             ]}
           />
           <Feature
@@ -36,7 +36,7 @@ export default function Features() {
             description="Automatic detailed logging of activities and resource access. Useful for both real-time and historical tracking"
             image="audit-logs.svg"
             bullets={[
-              "Quickly answer queries about who accessed what",
+              "Quickly answer questions about who accessed what",
               "View cluster-level history",
               "Leverage Kubernetes-aware filtering by cluster, namespace, access method, etc.",
             ]}
@@ -44,7 +44,7 @@ export default function Features() {
           <Feature
             title="Access through the web or CLI"
             reverse={true}
-            description="ZTKA can be installed using Helm, and ships with a browser-based GUI as well as traditional command-line tooling"
+            description="Paralus can be installed using Helm, and ships with a browser-based GUI as well as traditional command-line tooling"
             bullets={[
               "pctl tool to manage kubeconfigs directly from terminal",
               "Well-documented REST API",
